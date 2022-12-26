c7379e1af1c8050fedbda5952f149067ca29926d
fusionking <carsoy@collective.com>
2022-12-26 20:26:25 +0300

    SportSelection admin

--- selections/admin.py
+++ selections/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from selections.models import SportSelection, Slot, Selection
 
 # Register your models here.
-admin.site.register(SportSelection)
+
+
+class SportSelectionAdmin(admin.ModelAdmin):
+    list_display = ("branch_name", "complex_name", "pitch_name")
+    fields = ()
+    readonly_fields = ("branch_id", "complex_id", "pitch_id")
+
+
+admin.site.register(SportSelection, SportSelectionAdmin)
 admin.site.register(Slot)
 admin.site.register(Selection)
